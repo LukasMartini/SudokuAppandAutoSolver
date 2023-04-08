da65ba8392e5ebc4779a3aa1aae718b39db5d6fa
LukasMartini <60274604+LukasMartini@users.noreply.github.com>
2023-04-08 18:24:05 -0400

    Added backend features to prevent duplicate values in possibilities mode and auto-sorting of input values.

--- InputTable.cpp
+++ InputTable.cpp
@@ -11,14 +11,7 @@ InputTable::InputTable() {
     this->tableLayout = new QGridLayout;
     for (int i = 0; i < 81; i++) {
         auto* newBox = new QLineEdit;
-        // TODO: change font sizes and text locations to be approprate for mode
-        newBox->setStyleSheet({
-          "border : 2px solid black;"
-          "background : white;"
-          "height : 50px;"
-          "width : 50px;"
-          "color : black"
-        });
+        newBox->setStyleSheet(this->settingSS);
         newBox->setValidator(this->forceTheseValuesWhenSetting); // This is the default validator.
         connect(newBox, &QLineEdit::textChanged, this, &InputTable::returnUpdatedTileValue);
         this->tileInputBoxes[i] = newBox;
@@ -28,15 +21,46 @@ InputTable::InputTable() {
     this->tableLayout->setContentsMargins(0,0,0,0);
 }
 
+bool InputTable::validateLength(QString values) {
+    return (this->settingMode ? values.length() <= 1 : values.length() <= 9);
+}
+
+bool InputTable::validateNoDuplicates(std::string values) {
+    for (int i = 0; i < values.length(); i++) {
+        if (i != values.length() - 1 && values.at(i) == values.at(i+1)) {
+            return false;
+        }
+    }
+    return true;
+}
+
 int InputTable::returnUpdatedTileValue(QString newVal) {
+    // TODO: Allow for formatting based on input mode for passing to ST.
+    auto* box = qobject_cast<QLineEdit*>(sender());
+    if (this->settingMode && !this->validateLength(newVal)) {
+        QString newSetValue = newVal.at(newVal.length()-1);
+        box->setValidator(this->forceTheseValuesWhenSetting);
+        box->setStyleSheet(this->settingSS);
+        box->setText(newSetValue);
+    } else if (this->settingMode) { // NOTE: this should run if this->validateLength returns true. It was not included for brevity.
+        box->setStyleSheet(this->settingSS);
+    } else {
+        box->setStyleSheet(this->possibilitiesSS);
+        std::string sortableNewVal = newVal.toStdString(); // Had bad access issues, just converted to string instead.
+        sort(sortableNewVal.begin(), sortableNewVal.end()); // Sorts the string to make duplicate checking possible and cleans the UI.
+        if (!this->validateNoDuplicates(sortableNewVal)) {
+            box->setText(QString::fromStdString(sortableNewVal.substr(0, newVal.length()-1)));
+        } else {
+            box->setText(QString::fromStdString(sortableNewVal));
+        }
+    }
     return newVal.toInt();
 }
 
 void InputTable::switchMode() {
     this->settingMode = !this->settingMode;
-    std::cout << this->settingMode;
     for (auto & boxes : this->tileInputBoxes) {
-        if (this->settingMode) {
+        if (this->settingMode && this->validateLength(boxes.second->text())) {
             boxes.second->setValidator(this->forceTheseValuesWhenSetting);
         } else {
             boxes.second->setValidator(this->forceTheseValuesWhenPossibilitying);
--- InputTable.h
+++ InputTable.h
@@ -26,10 +26,25 @@ class InputTable : public QWidget{
         void switchMode();
 
     private:
+        bool validateLength(QString values);
+        bool validateNoDuplicates(std::string values);
+
         SudokuTable currentTable;
         std::map<int, QLineEdit*> tileInputBoxes;
         QRegularExpression settingRE {"^[1-9]{0,1}$"};
-        QRegularExpression possibilitiesRE {"^[1-9]{0,9}$"};
+        QString settingSS {"border : 2px solid black;"
+                               "background : white;"
+                               "height : 50px;"
+                               "width : 50px;"
+                               "color : black;"
+                               "font-size : 20px"};
+        QRegularExpression possibilitiesRE {"^[1-9]{0,9}$"}; // TODO: allow for an additional character when setting mode is true but validate is false.
+        QString possibilitiesSS { "border : 2px solid black;"
+                                  "background : white;"
+                                  "height : 50px;"
+                                  "width : 50px;"
+                                  "color : black;"
+                                  "font-size : 10px"};
         QValidator* forceTheseValuesWhenSetting = new QRegularExpressionValidator(settingRE, this);
         QValidator* forceTheseValuesWhenPossibilitying = new QRegularExpressionValidator(possibilitiesRE, this);
         bool settingMode = true; // inputMode being true means set number mode, false means possibilities mode.
